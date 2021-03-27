b258c612bf413d0921a6bc04af3feee1c4aa51e6
lhyjtz <lhyjtz@qq.com>
2021-03-27 16:46:11 +0800

    first commit

---
+++ src/components/MenuTree.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+  <template v-for="item in this.menuList">
+    <el-submenu  v-if="item.children.length>0"
+                 :disabled="!(item.disabled)"
+                 :index="item.id+''"
+                 :key="item.id">
+        <template slot="title">
+           <i :class="item.uri"></i>
+          <span>{{item.menuName}}</span>
+        </template>
+      <menu-tree :menu-list="item.children"></menu-tree>
+    </el-submenu>
+    <el-menu-item v-else
+                  :disabled="!(item.disabled)"
+                  :index="item.uri+''"
+                  :key="item.id"
+                  :route="item.uri+''"
+    >
+      <i :class="item.icon"></i>
+      <span>{{item.menuName}}</span>
+    </el-menu-item>
+  </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MenuTree",
+    props:["menuList",],
+  }
+</script>
+
+<style scoped>
+  .el-menu--collapse span
+  {
+     height: 0;
+    width: 0;
+    overflow: hidden;
+    visibility: hidden;
+    display: inline-block;
+  }
+</style>
